d81c3643ee4f807b1ed0b5eb8c8b3bcb73146278
davidduartedias <david.dias@unodigital.io>
2026-06-11 18:24:59 +0100

    changed to map() and alter return logic

--- 393.js
+++ 393.js
@@ -1,20 +1,44 @@
 // ob-393------------------------------------------------------------
+// function compressStringOB392(string1) {
+//   let finalStrg = "";
+
+//   let obj1 = [...string1].reduce((acc, val) => {
+//     acc[val] ? acc[val]++ : (acc[val] = 1);
+//     return acc;
+//   }, {});
+
+//   for (const [key, value] of Object.entries(obj1)) {
+//     finalStrg += key.concat(value);
+//   }
+
+//   if (finalStrg.length > string1.length) {
+//     return string1;
+//   }
+//   return finalStrg;
+// }
+// ob-393------------------------------------------------------------
 function compressStringOB392(string1) {
+  const strings = new Map();
   let finalStrg = "";
 
-  let obj1 = [...string1].reduce((acc, val) => {
-    acc[val] ? acc[val]++ : (acc[val] = 1);
-    return acc;
-  }, {});
-
-  for (const [key, value] of Object.entries(obj1)) {
-    finalStrg += key.concat(value);
+  for (let char of [...string1]) {
+    if (!strings.has(char)) {
+      strings.set(char, 1);
+    } else {
+      strings.set(char, strings.get(char) + 1);
+    }
   }
 
-  if (finalStrg.length > string1.length) {
-    return string1;
-  }
+  strings.forEach((value, key) => {
+    if (value > 1) {
+      finalStrg += key.concat(value);
+    } else {
+      finalStrg += key;
+    }
+  });
+
   return finalStrg;
 }
 console.log(compressStringOB392("aaabbccccd"));
 console.log(compressStringOB392("abcd"));
+console.log(compressStringOB392("aaab"));
