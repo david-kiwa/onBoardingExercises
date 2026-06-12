45ba00df9e83b4ba929ae9155e31393f29a5e89b
davidduartedias <david.dias@unodigital.io>
2026-06-12 16:37:37 +0100

    changed the code to incorporate remainder logic

--- 396.js
+++ 396.js
@@ -2,9 +2,14 @@
 function rotateArrOB396(newArr, k) {
   const arr = [...newArr];
 
-  let result = arr.splice(k > arr.length ? 1 : -k, arr.length);
+  let remainder = k % arr.length;
+
+  let rotation = k > arr.length ? -remainder : -k;
+
+  let result = arr.splice(rotation, arr.length);
 
   return [...result, ...arr];
 }
 console.log(rotateArrOB396([1, 2, 3, 4, 5, 6, 7], 3));
 console.log(rotateArrOB396([10, 20, 30], 5));
+console.log(rotateArrOB396([10, 20, 30, 40, 50, 60, 70, 80], 15));
